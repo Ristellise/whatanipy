c7313d9345a023347b3dffeb34e31ff7a938216f
Ristellise <joshwoo71@gmail.com>
2018-08-20 21:44:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Whatanime.ga python wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/example-project",
+    url="https://github.com/Ristellise/whatanipy",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
